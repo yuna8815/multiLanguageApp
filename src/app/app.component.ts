d75afdb898192d85a36f50eadf9c3b5bafb3872a
KimYuNa <hugyaa@gmail.com>
2023-01-31 00:30:11 +0900

    feat: 다국어 서비스 적용 테스트

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private translate: TranslateService
+  ) {
+    console.log(translate.getBrowserLang())
+    let lang = translate.getBrowserLang()!;
+    translate.setDefaultLang(lang);
+    translate.use(lang)
+  }
 }
